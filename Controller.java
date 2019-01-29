ef926ff2aee2341471c8f9c8b13cf60178ea9c9b
veonico <47023884+veonico@users.noreply.github.com>
2019-01-29 15:34:48 +0900

    Update Controller.java
    단위 맞춤

--- Controller.java
+++ Controller.java
@@ -8,19 +8,16 @@ import java.awt.event.WindowEvent;
 import com.mulcam.typing_game.view.InputForm;
 import com.mulcam.typing_game.view.MainView;
 import com.mulcam.typing_game.view.PlayView;
-import com.mulcam.typing_game.view.RankingView;
 
 public class Controller implements ActionListener {
 	MainView mainView;
 	InputForm form;
 	PlayView play;
-	RankingView rank;
-	
+
 	public Controller() {
 		mainView = new MainView();
 		form = new InputForm();
 		play = new PlayView();
-		rank = new RankingView();
 		eventUp();
 	}
 
@@ -34,10 +31,6 @@ public class Controller implements ActionListener {
 		// -----playview-----
 		play.btn_back.addActionListener(this);
 
-		// ----rainkingview-----
-		rank.btn_main.addActionListener(this);
-
-		
 		// 입력폼의 우측상단 'X'버튼 클릭시 메인화면으로 이동
 		form.addWindowListener(new WindowAdapter() {
 			@Override
@@ -73,10 +66,7 @@ public class Controller implements ActionListener {
 		if (ob == mainView.btn_rainfall) {// 메인뷰 비내리기 버튼 클릭
 			mainView.setVisible(false);
 			play.setVisible(true);
-		} else if (ob == play.btn_back) {// play뷰에서 메인뷰로 이동하는 버튼 클릭
-			mainView.setVisible(true);
-			play.setVisible(false);
-		} else if (ob == rank.btn_main) {// ranking뷰에서 메인뷰로 이동하는 버튼 클릭
+		} else if (ob == play.btn_back) {
 			mainView.setVisible(true);
 			play.setVisible(false);
 		}
