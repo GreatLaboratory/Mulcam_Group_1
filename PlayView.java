0ed7b625c44dd86b7195bc2eceda1c2b10cf051b
veonico <47023884+veonico@users.noreply.github.com>
2019-01-29 19:18:47 +0900

    Update PlayView.java
    thread 및 runnable 배열화
    Actionlistnener 수정

--- PlayView.java
+++ PlayView.java
@@ -1,4 +1,5 @@
-package game;
+package com.mulcam.typing_game.view;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
@@ -24,13 +25,12 @@ public class PlayView extends JFrame {
 //	
 	BorderLayout bd = new BorderLayout();
 //	
-	public WordsDAO dao;
 	public JButton btn_back;
 	JButton btn_over; // 일단 생성만하고 아래엔 주석처리해놓음
 //	
 	static int sec = 0; // 스테틱으로 해놔야됨
 	
-	boolean isitthefirst = true;
+	boolean isitthefirst = false;
 	boolean onlyfortimer = true;
 	
 	Drop d = new Drop();
@@ -38,8 +38,18 @@ public class PlayView extends JFrame {
 	Drop d2 = new Drop();
 	Drop d3 = new Drop();
 	
+	Drop [] d_array = {d, d1, d2, d3};
+	
+	Thread t = new Thread (d);
+	Thread t1 = new Thread (d1);
+	Thread t2 = new Thread (d2);
+	Thread t3 = new Thread (d3);
+	
+	Thread [] t_array = {t,t1,t2,t3};
+			
+	
 	public PlayView() {
-				dao = new WordsDAO();
+				
 		LowerPanel = new JPanel ();
 		UpperPanel = new JPanel ();
 		PlayPanel = new JPanel ();
@@ -69,27 +79,18 @@ public class PlayView extends JFrame {
 	
 	// PlayPanel=============================
 	
-	PlayPanel.setLayout(null);
-	   d.setBounds(0, 0, 100, 30);
-	  PlayPanel.add(d);
-	
-	  d1.setBounds(0, 0, 100, 30); // 두번째 쓰레드 생성만해놓음
-	  d1.setText(dao.select());
-	  PlayPanel.add(d1);
+	  PlayPanel.setLayout(null);
 	  
-	  d2.setBounds(0, 0, 100, 30); // 세번째 쓰레드 생성만해놓음
-	  d2.setText(dao.select());
-	  PlayPanel.add(d2);
-	  
-	  d3.setBounds(0, 0, 100, 30); // 네번째 쓰레드 생성만해놓음
-	  d3.setText(dao.select());
-	  PlayPanel.add(d3);
-	  
-	  new Thread(d).start(); // 첫번째 쓰레드 시작!
+	  for(Drop d : d_array) {
+		  d.setBounds(0,0,100,30);
+		  PlayPanel.add(d);
+		  d.setVisible(false);
+	  }
 
-		
-	
-	
+	  d1.setText("두번째다잉");
+	  d2.setText("세번째다잉");
+	  d3.setText("네번째다잉");
+	  
 	  la_notice = new JLabel("시작하려면 아무 글자나 입력해주세요");
 	  la_notice.setFont(new Font("굴림", Font.BOLD, 20));
 	  la_notice.setHorizontalAlignment(SwingConstants.CENTER);
@@ -113,32 +114,55 @@ public class PlayView extends JFrame {
 		public void actionPerformed(ActionEvent e) {
 			sec++;
 			// 시간 이렇게 정한 이유는 그래야 간격이 좀 맞는 것 같아서, 수정해도 ㄱㅊ
-			  if (PlayView.sec==30 )	  new Thread(d1).start();
-			  if (PlayView.sec==47 )	  new Thread(d2).start();
-			  if (PlayView.sec==60 )	  new Thread(d3).start();
+			  if (PlayView.sec==30 )	  
+			  {t1.start(); d1.setVisible(true);}
+			  if (PlayView.sec==47 )	  
+			  {t2.start(); d2.setVisible(true);}
+			  if (PlayView.sec==60 )	  
+			  {t3.start(); d3.setVisible(true);}
+//			  Thread t = new Thread(d);
+			  
 			  la_time.setText("" + sec);
 
 		if(sec<10) {la_time.setForeground(Color.red);};	
 		}
 	});
 	
-	Timer round_over= new Timer(10000,new ActionListener() {
-		
-		public void actionPerformed(ActionEvent e) {
-		//overButton.setVisible(true);
-
-		}
-	});
 	// Textfield ==============================
 
 	input_text.addActionListener(new ActionListener() {
 		
-		@Override
+		String written ;
+		
 		public void actionPerformed(ActionEvent e) {
+			
+			if(isitthefirst == false) {
+			 t.start(); // 첫번째 쓰레드 시작!
+			 isitthefirst = true;}
+			
+			 d.setVisible(true);
 			timer.start();
-			round_over.start();
 			la_notice.setVisible(false);
+			
+		//=====================================	
+			
+			
+			written = e.getActionCommand();
+			for( int i =0; i < 4; i++ ) {
+				
+			if(written.equals(d_array[i].getText())) {
+				
+				d_array[i].setLocation(0,0);
+//				d_array[i].setVisible(false);
+				d_array[i].setText("ㅎㅇ");
+				
+				t_array[i].interrupt();
+				t_array[i] = new Thread(d_array[i]);
+				t_array[i].start();
+				System.out.println(t_array[i].isInterrupted());
+			}
 			input_text.setText("");
+		}
 		}
 	});
 	//프레임 성격 ==============================
