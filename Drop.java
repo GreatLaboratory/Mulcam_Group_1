a33c01efe650b94e674b7c99f00d9ac9ad9221f0
veonico <47023884+veonico@users.noreply.github.com>
2019-01-29 19:17:55 +0900

    Update Drop.java
    interrupt 기능 사용하기 위해 while문 수정

--- Drop.java
+++ Drop.java
@@ -3,28 +3,29 @@ package com.mulcam.typing_game.view;
 import javax.swing.JLabel;
 
 public class Drop extends JLabel implements Runnable{
-	 WordsDAO dao ;
-	 
+	
+	int j =0;
+	
 	public Drop() {
-		dao = new WordsDAO();
-		setText(dao.select());
+		setText("하이");
 	}
-	@Override
+
 	public void run() {
 		
-		while (true) {
+		try {
+			
+		while (!Thread.currentThread().isInterrupted()) {
 			int i = (int) (Math.random()*550+1);
-			for (int j = 0; j < 700; j += 10) {
+			for (j = 0; j < 700; j += 10) {
 				
 				setLocation ( i ,j );
-				try {
-					Thread.sleep(100 - (PlayView.sec / 5) ); // 수정한 부분
-									// 5의 배수의 시간이 흐를 때마다 점점 속도증가
-					//System.out.println("ch");
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
-			}
+					Thread.sleep(100 - 2*( PlayView.sec / 5) );
+					//System.out.println("ch");			
+			}			
 		}
+		
+		} catch (Exception e) {		
 	}//run
+	}
 }
+
