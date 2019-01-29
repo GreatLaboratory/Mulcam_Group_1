420932dea1db045c3f1619f5e6e2a9bd30b1c43f
GreatLaboratory <46255148+GreatLaboratory@users.noreply.github.com>
2019-01-29 14:23:38 +0900

    mainview로 돌아가는 버튼생성

--- RankingView.java
+++ RankingView.java
@@ -10,6 +10,7 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JList;
+import javax.swing.JButton;
 
 public class RankingView extends JFrame {
 
@@ -17,6 +18,7 @@ public class RankingView extends JFrame {
 	JLabel la_ranking, la_first, la_second, la_third, la_forth;
 	JLabel la_first_id, la_second_id, la_third_id, la_forth_id;
 	JLabel la_first_score, la_second_score, la_third_score, la_forth_score;
+	public JButton btn_main;
 	/**
 	 * Create the frame.
 	 */
@@ -29,88 +31,92 @@ public class RankingView extends JFrame {
 		setContentPane(contentPane);
 		
 		
-		//------------------ø©±‰ πŸ≤Ó¡ˆæ ¥¬ ∞Ì¡§ πÆ¿⁄----------------------------
+		//------------------Ïó¨Í∏¥ Î∞îÎÄåÏßÄÏïäÎäî Í≥†Ï†ï Î¨∏Ïûê----------------------------
 		la_ranking = new JLabel("Ranking");
 		la_ranking.setHorizontalAlignment(SwingConstants.CENTER);
-		la_ranking.setFont(new Font("±º∏≤", Font.PLAIN, 60));
+		la_ranking.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 60));
 		la_ranking.setBounds(103, 72, 307, 115);
 		contentPane.add(la_ranking);
 		
 		 la_first = new JLabel("1st");
-		la_first.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_first.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_first.setHorizontalAlignment(SwingConstants.CENTER);
 		la_first.setBounds(14, 231, 99, 64);
 		contentPane.add(la_first);
 		
 		 la_second = new JLabel("2nd");
 		la_second.setHorizontalAlignment(SwingConstants.CENTER);
-		la_second.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_second.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_second.setBounds(24, 307, 89, 64);
 		contentPane.add(la_second);
 		
 		 la_third = new JLabel("3rd");
 		la_third.setHorizontalAlignment(SwingConstants.CENTER);
-		la_third.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_third.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_third.setBounds(34, 383, 79, 64);
 		contentPane.add(la_third);
 		
 		 la_forth = new JLabel("4th");
 		la_forth.setHorizontalAlignment(SwingConstants.CENTER);
-		la_forth.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_forth.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_forth.setBounds(24, 459, 99, 64);
 		contentPane.add(la_forth);
 		
-		//------------------DBø°º≠ ID πﬁæ∆ø¿±‚-----------------------
+		//------------------DBÏóêÏÑú ID Î∞õÏïÑÏò§Í∏∞-----------------------
 		la_first_id = new JLabel("ID");
-		la_first_id.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_first_id.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_first_id.setHorizontalAlignment(SwingConstants.CENTER);
 		la_first_id.setBounds(127, 231, 207, 64);
 		contentPane.add(la_first_id);
 		
 		la_second_id = new JLabel("ID");
 		la_second_id.setHorizontalAlignment(SwingConstants.CENTER);
-		la_second_id.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_second_id.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_second_id.setBounds(127, 307, 207, 64);
 		contentPane.add(la_second_id);
 		
 		la_third_id = new JLabel("ID");
 		la_third_id.setHorizontalAlignment(SwingConstants.CENTER);
-		la_third_id.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_third_id.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_third_id.setBounds(127, 383, 207, 64);
 		contentPane.add(la_third_id);
 		
 		la_forth_id = new JLabel("ID");
 		la_forth_id.setHorizontalAlignment(SwingConstants.CENTER);
-		la_forth_id.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_forth_id.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_forth_id.setBounds(127, 459, 207, 64);
 		contentPane.add(la_forth_id);
 		
-		//-----------------------DBø°º≠ SCORE πﬁæ∆ø¿±‚---------------------------
+		//-----------------------DBÏóêÏÑú SCORE Î∞õÏïÑÏò§Í∏∞---------------------------
 		la_first_score = new JLabel("SCORE");
 		la_first_score.setHorizontalAlignment(SwingConstants.CENTER);
-		la_first_score.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_first_score.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_first_score.setBounds(336, 231, 148, 64);
 		contentPane.add(la_first_score);
 		
 		la_second_score = new JLabel("SCORE");
 		la_second_score.setHorizontalAlignment(SwingConstants.CENTER);
-		la_second_score.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_second_score.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_second_score.setBounds(336, 307, 148, 64);
 		contentPane.add(la_second_score);
 		
 		la_third_score = new JLabel("SCORE");
 		la_third_score.setHorizontalAlignment(SwingConstants.CENTER);
-		la_third_score.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_third_score.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_third_score.setBounds(336, 383, 148, 64);
 		contentPane.add(la_third_score);
 		
 		la_forth_score = new JLabel("SCORE");
 		la_forth_score.setHorizontalAlignment(SwingConstants.CENTER);
-		la_forth_score.setFont(new Font("±º∏≤", Font.PLAIN, 30));
+		la_forth_score.setFont(new Font("Íµ¥Î¶º", Font.PLAIN, 30));
 		la_forth_score.setBounds(336, 459, 148, 64);
 		contentPane.add(la_forth_score);
 		
+		btn_main = new JButton("mainÏúºÎ°ú ÎèåÏïÑÍ∞ÄÍ∏∞");
+		btn_main.setBounds(160, 603, 184, 36);
+		contentPane.add(btn_main);
 		
-		setVisible(false);
+		
+		setVisible(true);
 	}
 }
