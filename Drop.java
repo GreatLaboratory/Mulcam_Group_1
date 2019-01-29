c210504187979844e8cf468855a2b31012c1b9ee
wngh0908 <38426656+wngh0908@users.noreply.github.com>
2019-01-29 21:39:19 +0900

    드디어!

--- Drop.java
+++ Drop.java
@@ -2,30 +2,36 @@ package com.mulcam.typing_game.view;
 
 import javax.swing.JLabel;
 
-public class Drop extends JLabel implements Runnable{
+public class Drop extends JLabel implements Runnable {
+	WordsDAO dao;
 	
+//	int i = (int) (Math.random() * 550 + 1);
 	int j =0;
 	
 	public Drop() {
-		setText("하이");
+		dao = new WordsDAO();
+		setText(dao.select());
+		
 	}
 
+	@Override
 	public void run() {
-		
 		try {
-			
 		while (!Thread.currentThread().isInterrupted()) {
-			int i = (int) (Math.random()*550+1);
-			for (j = 0; j < 700; j += 10) {
-				
-				setLocation ( i ,j );
-					Thread.sleep(100 - 2*( PlayView.sec / 5) );
-					//System.out.println("ch");			
-			}			
-		}
-		
-		} catch (Exception e) {		
-	}//run
-	}
-}
+			int i = (int) (Math.random() * 550 + 1);
+			for ( j = 0; j < 700; j += 1) {
 
+				setLocation(i, j);
+				
+					Thread.sleep(100- 5*(PlayView.sec / 5)); // 수정한 부분
+//					Thread.sleep(200 - (PlayView.sec / 5) ); // 수정한 부분
+					// 5의 배수의 시간이 흐를 때마다 점점 속도증가
+					// System.out.println("ch");
+			}}
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+
+			}
+		} // run
+	
