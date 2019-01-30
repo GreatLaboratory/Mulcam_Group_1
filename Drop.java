8325d00ecee2bd3eb2c96a2d27c525aa494a3191
hm0158 <42127568+hm0158@users.noreply.github.com>
2019-01-30 14:41:04 +0900

    Update Drop.java

--- Drop.java
+++ Drop.java
@@ -2,28 +2,35 @@ package com.mulcam.typing_game.view;
 
 import javax.swing.JLabel;
 
+import com.mulcam.typing_game.model.WordsDAO;
+
+import calculating.Cal;
+
 public class Drop extends JLabel implements Runnable {
 	WordsDAO dao;
 	
 //	int i = (int) (Math.random() * 550 + 1);
 	int j =0;
-	
-	public Drop() {
+	Cal c ;
+	public Drop(Cal c) {
+		this.c= c;
 		dao = new WordsDAO();
 		setText(dao.select());
 		
 	}
 
 	@Override
-	public void run() {
+	public void run() {//단어 떨어진다.
 		try {
 		while (!Thread.currentThread().isInterrupted()) {
 			int i = (int) (Math.random() * 550 + 1);
-			for ( j = 0; j < 700; j += 1) {
-
-				setLocation(i, j);
+			for ( j = 0; j < 620; j += 1) {
+				if(j == 580) {
+					c.life_cut();
+				}
+				    setLocation(i, j);
 				
-					Thread.sleep(100- 5*(PlayView.sec / 5)); // 수정한 부분
+				    Thread.sleep(100- 5*(PlayView.sec / 5)); // 수정한 부분
 //					Thread.sleep(200 - (PlayView.sec / 5) ); // 수정한 부분
 					// 5의 배수의 시간이 흐를 때마다 점점 속도증가
 					// System.out.println("ch");
@@ -34,4 +41,3 @@ public class Drop extends JLabel implements Runnable {
 
 			}
 		} // run
-	
