23cdac955f8ea4ba0bc1f4c527f64155566bf314
wngh0908 <38426656+wngh0908@users.noreply.github.com>
2019-01-30 11:40:39 +0900

    랭킹뷰 버튼 메인과 연결!!

--- Controller.java
+++ Controller.java
@@ -1,4 +1,6 @@
 package com.mulcam.typing_game.control;
+package game;
+
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,31 +18,36 @@ import game.*;
 
 public class Controller implements ActionListener {
 	MainView mainView;
-	InputForm form;
+	//InputForm form;
 	PlayView play;
-
+	RankingView rank; // 랭킹 
+	
 	public Controller() {
 		mainView = new MainView();
-		form = new InputForm();
+	//	form = new InputForm();
 		play = new PlayView();
+		rank = new RankingView(); // 랭킹
 		eventUp();
 	}
 
 	private void eventUp() {// 이벤트 등록
+		
 		// ----mainView----
 		//mainView.btn_input.addActionListener(this);        ---  정보입력 버튼 / 나중에 게임 끝나고 불러오자
 		mainView.btn_rainfall.addActionListener(this);
 
-		// ----inputForm----
-		form.bt_submit.addActionListener(this);
+		// ----rankingView----
+		mainView.btn_rank.addActionListener(this); // 메인에서 랭킹 볼 수 있게 추가해놈
+		rank.btn_main.addActionListener(this);
+		
 		// -----playview-----
 		play.btn_back.addActionListener(this);
 
-		// 입력폼의 우측상단 'X'버튼 클릭시 메인화면으로 이동
-		form.addWindowListener(new WindowAdapter() {
+		// 랭킹뷰의 우측상단 'X'버튼 클릭시 메인화면으로 이동
+		rank.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
-				form.setVisible(false);
+				rank.setVisible(false);
 				mainView.setVisible(true);
 			}
 		});
@@ -49,25 +56,14 @@ public class Controller implements ActionListener {
 	public void actionPerformed(ActionEvent e) {// 이벤트 처리부(요청 분석)
 		Object ob = e.getSource();// 이벤트 소스의 레퍼런스 얻어오기
 		
-		
-//		if (ob == mainView.btn_input) {//                  메인뷰 입력버튼 클릭 / 나중에 게임 끝나고 불러오자
-//			mainView.setVisible(false);
-//			form.setVisible(true);
-//		} else if (ob == form.bt_submit) {// 입력폼 입력버튼 클릭
-//			// from : 입력폼 뷰, to : DB
-//			String id = form.tf_id.getText();
-//			int score = 0;
-//			GamerVO vo = new GamerVO(id, score);
-//
-//			GamerDAO dao = new GamerDAO();
-//			if (dao.insert(vo)) {
-//				form.showMsg("입력성공!!");
-//				form.setVisible(false);
-//				mainView.setVisible(true);
-//			}
-//		} 
-		
-		
+		if (ob == mainView.btn_rank) {// 랭킹 버튼 클릭
+			mainView.setVisible(false);
+			rank.setVisible(true);
+		} 
+		else if (ob == rank.btn_main) { 
+			mainView.setVisible(true);
+			rank.setVisible(false);
+		}
 		if (ob == mainView.btn_rainfall) {// 메인뷰 비내리기 버튼 클릭
 			mainView.setVisible(false);
 			play.setVisible(true);
