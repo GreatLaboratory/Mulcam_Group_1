616e1c2668994a297992ff47c1454ba3bfbfcccf
GreatLaboratory <46255148+GreatLaboratory@users.noreply.github.com>
2019-01-31 10:50:47 +0900

    db에 상위 4개 데이터만 select해오는 메소드 추가

--- GamerDAO.java
+++ GamerDAO.java
@@ -1,12 +1,18 @@
 package com.mulcam.typing_game.model;
 
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 
 import mybatis.MySqlSession;
 
-public class GamerDAO {// DBÀü´ã Å¬·¡½º (DAO: DataBase Access Object)
-	// ---> CRUD ÀÛ¾÷ Create(insert) Read(select) Update Delete
+public class GamerDAO {// DBì „ë‹´ í´ë˜ìŠ¤ (DAO: DataBase Access Object)
+	// ---> CRUD ì‘ì—… Create(insert) Read(select) Update Delete
 
+	public String first_id, second_id, third_id, forth_id;
+//	public int first_score, second_score, third_score, forth_score;
+	public String first_score, second_score, third_score, forth_score;
 	SqlSession session;
 
 	public GamerDAO() {
@@ -14,9 +20,38 @@ public class GamerDAO {// DBÀü´ã Å¬·¡½º (DAO: DataBase Access Object)
 	}
 
 	public boolean insert(GamerVO vo) {
-		// session.insert("³×ÀÓ½ºÆäÀÌ½º¸í.¾ÆÀÌµğ¸í", Object parameter);
+		// session.insert("ë„¤ì„ìŠ¤í˜ì´ìŠ¤ëª….ì•„ì´ë””ëª…", Object parameter);
 		session.insert("gamer.insert", vo);
 		session.commit();
 		return true;
+	}
+	
+	public List<GamerVO> select() {
+		//GamerVO vo = new GamerVO();
+		//session.selectOne("gamer.selectOne");  // ê²°ê³¼í–‰ì´ í•˜ë‚˜ì˜ í–‰ì¼ ë•Œ
+		RowBounds bounds = new RowBounds(0, 4);
+		//session.selectList("íŒ¨í‚¤ì§€ëª….ì•„ì´ë””ëª…",Object param, bounds);
+		//List<GamerVO> list =session.selectList("gamer.select",null, bounds);
+		return session.selectList("gamer.select",null, bounds);
+		
+//		for(GamerVO vo:list) {
+//			System.out.println("id>>>"+vo.getId());
+//		}
+		
+//		this.first_id = list.get(0).getId();
+//		this.second_id = list.get(1).getId();
+//		this.third_id = list.get(2).getId();
+//		this.forth_id = list.get(3).getId();
+		
+//		this.first_score = Integer.toString(list.get(0).getScore());
+//		this.first_score = Integer.toString(list.get(1).getScore());
+//		this.first_score = Integer.toString(list.get(2).getScore());
+//		this.first_score = Integer.toString(list.get(3).getScore());
+//		
+		
+		//List<String> list = session.selectList("names.selectAll"); // ê²°ê³¼í–‰ì´ ë‘ ê°œ ì´ìƒì¼ ë•Œ
+//		for(String e:list) {
+//			System.out.println(" "+e);
+//		}
 	}
 }
